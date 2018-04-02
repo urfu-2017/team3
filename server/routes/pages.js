648f853aba8e883fd2e807dd4af620bdf1aedde7
Unknown <r.artem74@gmail.com>
2018-04-02 22:25:56 +0500

    add enshure login middleware

--- server/routes/pages.js
+++ server/routes/pages.js
@@ -1,15 +1,14 @@
 'use strict';
 
+const connectEnsureLogin = require('connect-ensure-login');
+
 module.exports = (server, app) => {
     server
         .get('/', (req, res) => {
             app.render(req, res, '/index');
         })
-        .get('/profile', (req, res) => {
-            if (req.user) {
-                app.render(req, res, '/profile');
-            } else {
-                res.redirect('/');
-            }
-        });
+        .get('/profile',
+            connectEnsureLogin.ensureLoggedIn('/'),
+            (req, res) => app.render(req, res, '/profile')
+        );
 };
