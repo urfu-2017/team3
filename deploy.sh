cd009a7d6370c2435dff12e8bf8a575e3ae2e223
Unknown <r.artem74@gmail.com>
2018-04-07 01:42:54 +0500

    set token for now commands

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ firstArg=$1
 buildAlias='team-3-'${firstArg}'.now.sh'
 buildPostfixPattern=${firstArg}'.now.sh\>'
 
-currentTestBuild=$(now alias ls | grep $buildPostfixPattern | awk '{print $1}')
+currentTestBuild=$(now alias ls --token $NOW_TOKEN | grep $buildPostfixPattern | awk '{print $1}')
 if [ ! -z $currentTestBuild ]
 then
     echo 'start remove old build ' $currentTestBuild
@@ -13,5 +13,5 @@ fi
 echo 'start build'
 echo $(now --public -e NODE_ENV=production --token $NOW_TOKEN --npm --dotenv=.env.production)
 echo 'start creating alias'
-newBuild=$(now ls | head -5 | tail -1 | awk '{print $1}')
+newBuild=$(now ls --token $NOW_TOKEN | head -5 | tail -1 | awk '{print $1}')
 echo $(now alias $newBuild $buildAlias)
