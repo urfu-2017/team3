2b6c594c74e77c49a74c18cde0f818636c23947e
Paul Khomich <paulkhomich@outlook.com>
2018-04-09 13:51:56 +0500

    eslint

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const withCSS = require('@zeit/next-css')
+// const withCSS = require('@zeit/next-css')
 
-// Дополнительная настройка Next.js
-// Позволяет использовать css
-// Подробнее: https://github.com/zeit/next-plugins/tree/master/packages/next-css
-module.exports = withCSS({ useFileSystemPublicRoutes: false });
+// module.exports = withCSS({ useFileSystemPublicRoutes: false });
