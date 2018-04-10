f4642abbc50227c3c5bf5bd3b625bad64c12f3ec
Paul Khomich <paulkhomich@outlook.com>
2018-04-11 00:19:51 +0500

    Логика сообщений

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 
 /* eslint-disable */
 
-withCSS = require('@zeit/next-css')
+const withCSS = require('@zeit/next-css')
 
-module.exports = withCSS({ useFileSystemPublicRoutes: false });
+module.exports = withCSS({
+    useFileSystemPublicRoutes: false,
+    webpack(config, { dev }) {
+        if(dev) {
+            config.devtool = 'cheap-module-eval-source-map'
+        }
+        return config;
+    } 
+});
