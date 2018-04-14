b741b0047214c31a719053d6a75ed408a3a50f45
ArtyMgn <r.artem74@gmail.com>
2018-04-15 04:30:22 +0500

    add fetching meta from link

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,14 +1,32 @@
 'use strict';
 
 const uuid = require('uuid/v4');
+const getUrls = require('get-urls');
+const got = require('got');
+const metascraper = require('metascraper');
 
 class Message {
-    constructor(userId, content) {
+    constructor(userId, content, meta) {
         this.id = uuid();
         this.content = content;
         this.author = userId;
         this.date = Date.now();
-        this.meta = { };
+        this.meta = meta;
+    }
+
+    static async extractMeta(message) {
+        const urlsSet = getUrls(message);
+
+        if (urlsSet.size === 0) {
+            return {};
+        }
+
+        for (const targetUrl of urlsSet) {
+            const { body: html, url } = await got(targetUrl);
+            const metadata = await metascraper({ html, url });
+
+            return metadata;
+        }
     }
 
     save(dbclient, chatId) {
