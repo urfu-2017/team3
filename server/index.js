737910d0357e39f7631f712f4d50c1dfca4553cf
Unknown <r.artem74@gmail.com>
2018-04-21 15:24:08 +0500

    add mongo db connection

--- server/index.js
+++ server/index.js
@@ -4,12 +4,14 @@ require('dotenv').config();
 
 const next = require('next');
 const expressSession = require('express-session');
+const mongoose = require('mongoose');
 
 const setupPagesRoutes = require('./routes/pages');
 const setupApiRoutes = require('./routes/api');
 const setupAuthRoutes = require('./routes/auth');
 const passport = require('./github-authorization');
 
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING);
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const server = require('./server');
 
