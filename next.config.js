146e945ceb6f75e5aa5031be996d15c612019560
Artem Borzov <artem.borz@yandex.ru>
2018-05-03 23:03:55 +0500

    merge css in one file

--- next.config.js
+++ next.config.js
@@ -3,10 +3,25 @@
 /* eslint-disable no-undef */
 
 const withCSS = require('@zeit/next-css');
+const MergeFilesPlugin = require('merge-files-webpack-plugin');
 
 module.exports = withCSS({
     useFileSystemPublicRoutes: false,
-    webpack(config, { dev }) {
+    webpack(config, options) {
+        const { isServer, dev } = options;
+
+        if (!isServer) {
+            // Merge all CSS in one file
+            options.extractCSSPlugin.filename = 'static/[name].css';
+            config.plugins.push(
+                new MergeFilesPlugin({
+                    filename: 'static/style.css',
+                    test: /\.css/,
+                    deleteSourceFiles: true
+                })
+            );
+        }
+
         if (dev) {
             config.devtool = 'cheap-module-eval-source-map';
         }
