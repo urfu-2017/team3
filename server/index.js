5155c452f99383c052e28235114531615c86a8a6
ArtyMgn <r.artem74@gmail.com>
2018-05-03 23:19:24 +0500

    solve merge conflicts

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const next = require('next');
 const expressSession = require('express-session');
 const mongoose = require('mongoose');
+const cloudinary = require('cloudinary');
 
 const setupPagesRoutes = require('./routes/pages');
 const setupApiRoutes = require('./routes/api');
@@ -12,6 +13,13 @@ const setupAuthRoutes = require('./routes/auth');
 const passport = require('./github-authorization');
 
 mongoose.connect(process.env.DATABASE_CONNECTION_STRING);
+
+cloudinary.config({
+    'cloud_name': 'team3',
+    'api_key': process.env.CLOUDINARY_API_KEY,
+    'api_secret': process.env.CLOUDINARY_API_SECRET
+});
+
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const server = require('./server');
 const httpServer = require('http').Server(server);
