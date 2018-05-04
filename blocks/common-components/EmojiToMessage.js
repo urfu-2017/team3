92b7a759f55aef5ea3b85f508522a0bf117fe154
Paul Khomich <paulkhomich@outlook.com>
2018-05-04 23:33:14 +0500

    .

---
+++ blocks/common-components/EmojiToMessage.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Picker } from 'emoji-mart';
+
+import '../../node_modules/emoji-mart/css/emoji-mart.css';
+import { connect } from 'react-redux';
+
+class Emoji extends Component {
+    render() {
+        const { showEmojiToMsg, addEmoji } = this.props;
+
+        if (!showEmojiToMsg) {
+            return <div />;
+        }
+
+        const paletteStyle = {
+            position: 'absolute',
+            bottom: 'calc(48px + 5px)',
+            right: 'calc(32px + 16px + 24px)',
+            width: '25%',
+            minWidth: '300px'
+        };
+
+        return (
+            <Picker
+                set="emojione"
+                onSelect={addEmoji}
+                emoji="point_up"
+                style={paletteStyle}
+                showPreview={false}
+                showSkinTones={false}
+                autoFocus
+            />
+        );
+    }
+}
+
+Emoji.propTypes = {
+    showEmojiToMsg: PropTypes.bool,
+    addEmoji: PropTypes.func
+};
+
+export default connect(
+    state => ({
+        showEmoji: state.showEmojiToMsg
+    })
+)(Emoji);
