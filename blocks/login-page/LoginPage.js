76e94ccb67e9b5108df5bcd2d0bf5f66ff8638e4
LazzyMind <hazipov.vlad@yandex.ru>
2018-05-06 00:04:59 +0500

    Дизайн

--- blocks/login-page/LoginPage.js
+++ blocks/login-page/LoginPage.js
@@ -24,13 +24,23 @@ export default class LoginPage extends Component {
                     <title>K1logram</title>
                 </head>
                 <main className="log-in">
-                    <header className="log-in__welcome">
-                        <p className="log-in__welcome-text">
-                        Добро пожаловать в наш мессенджер!<br />K1logram -
-                        общение без границ.
-                        </p>
-                    </header>
+                    <img className="log-in__logo" src="static/main-label.svg" alt="Логотип" />
                     <Button btnParams={logIn} />
+                    <p className="log-in__team3 team3_1">Смайлы</p>
+                    <p className="log-in__team3 team3_2">Возможность загрузки своей аватарки</p>
+                    <p className="log-in__team3 team3_3">Список контактов</p>
+                    <p className="log-in__team3 team3_4">Drag&prime;n&prime;Drop фоток</p>
+                    <p className="log-in__team3 team3_5">Профиль пользователя</p>
+                    <p className="log-in__team3 team3_6">Микроразметка</p>
+                    <p className="log-in__team3 team3_7">Авторизация через Github</p>
+                    <p className="log-in__team3 team3_8">Подтягивание meta-данных</p>
+                    <p className="log-in__team3 team3_9">Прикрепление фоток</p>
+                    <p className="log-in__team3 team3_10">Интерфейс чата</p>
+                    <p className="log-in__team3 team3_11">Emoji</p>
+                    <p className="log-in__team3 team3_12">Аватарки как на GitHub</p>
+                    <p className="log-in__team3 team3_13">Обмен сообщениями</p>
+                    <p className="log-in__team3 team3_14">Групповые чаты</p>
+                    <p className="log-in__team3 team3_15">Инвайты в группу и в личный чат</p>
                 </main>
             </React.Fragment>
         );
