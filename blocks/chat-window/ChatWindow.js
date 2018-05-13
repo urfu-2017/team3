e805e593f0db0a4a082fd818b1eb82d1fa90095e
spt30 <seregatoschilin@gmail.com>
2018-05-13 20:45:03 +0500

    input вынесен в компонент

--- blocks/chat-window/ChatWindow.js
+++ blocks/chat-window/ChatWindow.js
@@ -11,19 +11,14 @@ import { connect } from 'react-redux';
 
 import { showProfile } from '../../actions/modals';
 import {
-    showEmoji,
     hideEmoji,
-    resetAttachments,
     addAttachments,
-    sendMessage,
-    showInputPopup,
     hideInputPopup } from '../../actions/activeChat';
 
 import Chat from '../../models/Chat';
 
-import InputPopup from './input-form/popup-additional-functions/InputPopup';
+import Input from './input-form/Input';
 import Message from './message/Message';
-import Emoji from './input-form/Emoji';
 import Preview from './preview-panel/Preview';
 
 import './ChatWindow.css';
@@ -32,15 +27,16 @@ class ChatWindow extends Component {
     constructor(props) {
         super(props);
         this.state = {
+<<<<<<< HEAD
             msgText: '',
             isDraggable: false,
             isRecord: false,
             recognizer: null
+=======
+            isDraggable: false
+>>>>>>> input вынесен в компонент
         };
     }
-
-    // при вводе добавляем с state
-    changeText = e => this.setState({ msgText: e.target.value });
 
     componentDidMount() {
         document.addEventListener('keydown', e => {
@@ -75,59 +71,6 @@ class ChatWindow extends Component {
 
         this.setState({ isDraggable: false });
         this.props.addAttachments(files);
-    };
-
-    // клик на рожицу, используется в <Emoji.../>
-    toggleEmoji = () => {
-        if (this.props.emojiActive) {
-            this.props.hideEmoji();
-        } else {
-            this.props.showEmoji();
-        }
-    };
-
-    // функция добавления emoji
-    addEmoji = emoji => {
-        const input = document.querySelector('.chat-input__write-field');
-        let currentValue = input.value;
-
-        currentValue += `${emoji.colons}`;
-        this.setState({ msgText: currentValue });
-        this.textInput.focus();
-        // input.value = currentValue;
-    };
-
-    /* eslint-disable max-statements */
-    submitMessage = () => {
-        const { attachments } = this.props;
-
-        if (this.state.msgText.trim() || attachments.length) {
-            this.props.resetAttachments();
-            this.setState({
-                msgText: ''
-            });
-            this.props.hideEmoji();
-            const input = document.querySelector('.chat-input__write-field');
-            const text = input.value;
-
-            input.value = '';
-
-            const message = {
-                text,
-                author: this.props.user.nickname,
-                attachments: this.props.attachments.map(a => a.url)
-            };
-            const chatId = this.props.activeChat._id;
-
-            this.props.sendMessage(chatId, message);
-        }
-    };
-
-    // прослушка отправки на Enter
-    keySubmitMessage = e => {
-        if (e.keyCode === 13) {
-            this.submitMessage();
-        }
     };
 
     showProfile = profile => {
@@ -253,7 +196,6 @@ class ChatWindow extends Component {
                         </div>
                     )
                 }
-                <Emoji addEmoji={this.addEmoji} />
                 <Preview />
                 <div className="chat-input" onClick={this.props.hideEmoji}>
                     <input
@@ -306,6 +248,7 @@ class ChatWindow extends Component {
                     />
                 </div>
                 <InputPopup />
+                <Input activeChat={this.props.activeChat} />
             </section>
         );
     }
@@ -313,34 +256,24 @@ class ChatWindow extends Component {
 
 ChatWindow.propTypes = {
     user: PropTypes.object,
-    emojiActive: PropTypes.bool,
-    showProfile: PropTypes.func,
-    showEmoji: PropTypes.func,
-    hideEmoji: PropTypes.func,
     activeChat: PropTypes.object,
+
+    showProfile: PropTypes.func,
     attachments: PropTypes.array,
-    resetAttachments: PropTypes.func,
-    showInputPopup: PropTypes.func,
+    addAttachments: PropTypes.func,
     hideInputPopup: PropTypes.func,
-    sendMessage: PropTypes.func,
-    addAttachments: PropTypes.func
+    hideEmoji: PropTypes.func
 };
 
 export default connect(
     state => ({
-        activeChat: state.chats.find(c => state.activeChat && c._id === state.activeChat.id),
         user: state.user,
-        emojiActive: state.activeChat && state.activeChat.showEmoji,
-        attachments: state.activeChat && state.activeChat.attachments,
-        showInputPopup: state.activeChat && state.activeChat.showInputPopup
+        activeChat: state.chats.find(c => state.activeChat && c._id === state.activeChat.id),
+        attachments: state.activeChat && state.activeChat.attachments
     }), {
         showProfile,
-        showEmoji,
-        hideEmoji,
-        sendMessage,
-        resetAttachments,
-        showInputPopup,
+        addAttachments,
         hideInputPopup,
-        addAttachments
+        hideEmoji
     }
 )(ChatWindow);
---
+++ blocks/chat-window/input-form/Input.js
@@ -0,0 +1,178 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-closing-bracket-location */
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable react/self-closing-comp */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import {
+    sendMessage,
+    resetAttachments,
+    addAttachments,
+    showInputPopup,
+    showEmoji,
+    hideEmoji } from '../../../actions/activeChat';
+
+import Emoji from './Emoji';
+import InputPopup from './popup-additional-functions/InputPopup';
+
+import './Input.css';
+
+// import './InputPopup.css';
+
+class Input extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            msgText: ''
+        };
+    }
+
+    // при вводе добавляем с state
+    changeText = e => this.setState({ msgText: e.target.value });
+
+    // клик на рожицу, используется в <Emoji.../>
+    toggleEmoji = () => {
+        if (this.props.emojiActive) {
+            this.props.hideEmoji();
+        } else {
+            this.props.showEmoji();
+        }
+    };
+
+    showPopup = e => {
+        e.stopPropagation();
+
+        this.props.showInputPopup();
+    }
+
+    // функция добавления emoji
+    addEmoji = emoji => {
+        const input = document.querySelector('.chat-input__write-field');
+        let currentValue = input.value;
+
+        currentValue += `${emoji.colons}`;
+        this.setState({ msgText: currentValue });
+        this.textInput.focus();
+    };
+
+    /* eslint-disable max-statements */
+    submitMessage = () => {
+        console.log(this.props);
+        const { attachments } = this.props;
+
+        if (this.state.msgText.trim() || attachments.length) {
+            this.props.resetAttachments();
+            this.setState({
+                msgText: ''
+            });
+            const input = document.querySelector('.chat-input__write-field');
+            const text = input.value;
+
+            input.value = '';
+
+            const message = {
+                text,
+                author: this.props.user.nickname,
+                attachments: this.props.attachments.map(a => a.url)
+            };
+            const chatId = this.props.activeChat._id;
+
+            this.props.sendMessage(chatId, message);
+        }
+    };
+
+    // прослушка отправки на Enter
+    keySubmitMessage = e => {
+        if (e.keyCode === 13) {
+            this.submitMessage();
+        }
+    };
+
+    // добавляем новые файлы в превью
+    onFilesChange = async e => {
+        const { currentTarget: { files } } = e;
+
+        await this.props.addAttachments(files);
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="chat-input">
+                    <input
+                        onChange={this.changeText}
+                        onKeyDown={this.keySubmitMessage}
+                        type="text"
+                        ref={input => { this.textInput = input; }}
+                        autoFocus
+                        className="chat-input__write-field"
+                        value={this.state.msgText}
+                    />
+                    <label
+                        className="chat-input__audioinput-btn chat-input__button"
+                        title="Набор голосом"
+                        >
+                    </label>
+                    <label
+                        className="chat-input__emoji-btn chat-input__button"
+                        onClick={event => event.stopPropagation()}
+                        title="Добавить emoji"
+                        >
+                        <input
+                            type="button"
+                            onClick={this.toggleEmoji}
+                            className="chat-input__input_not-visual"
+                        />
+                    </label>
+                    <label
+                        className="chat-input__burger-btn chat-input__button"
+                        onClick={this.showPopup}
+                        title="Прикрепить"
+                        >
+                    </label>
+                    <label
+                        src="/static/send_message.svg"
+                        onClick={this.submitMessage}
+                        className="chat-input__send-btn chat-input__button"
+                        title="Отправить сообщение"
+                    />
+                </div>
+                <Emoji addEmoji={this.addEmoji} />
+                <InputPopup />
+            </React.Fragment>
+        );
+    }
+}
+
+Input.propTypes = {
+    user: PropTypes.object,
+    activeChat: PropTypes.object,
+
+    sendMessage: PropTypes.func,
+    resetAttachments: PropTypes.func,
+    attachments: PropTypes.array,
+    showInputPopup: PropTypes.func,
+    addAttachments: PropTypes.func,
+    emojiActive: PropTypes.bool,
+    showEmoji: PropTypes.func,
+    hideEmoji: PropTypes.func
+};
+
+export default connect(
+    state => ({
+        showInputPopup: state.activeChat && state.activeChat.showInputPopup,
+        emojiActive: state.activeChat && state.activeChat.showEmoji,
+        user: state.user,
+        attachments: state.activeChat && state.activeChat.attachments
+    }), {
+        sendMessage,
+        resetAttachments,
+        addAttachments,
+        showEmoji,
+        hideEmoji,
+        showInputPopup
+    }
+)(Input);
