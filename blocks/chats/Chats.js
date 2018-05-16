858be0a2d05b9b2d64e8ea8783c9bbaaa111ec81
spt30 <seregatoschilin@gmail.com>
2018-05-16 10:51:33 +0500

    сделал подсветку подсказки через state

--- blocks/chats/Chats.js
+++ blocks/chats/Chats.js
@@ -7,18 +7,6 @@ import { connect } from 'react-redux';
 import ChatIcon from './ChatIcon';
 
 class Chats extends React.Component {
-
-    componentDidMount() {
-        const [, blinkImg] = document.querySelectorAll('.control-img');
-
-        if (this.props.chats.length === 0) {
-            blinkImg.classList.add('controls_zindex_max');
-            blinkImg.addEventListener('mouseup', e => {
-                e.target.classList.remove('controls_zindex_max');
-            });
-        }
-    }
-
     render() {
         const { chats } = this.props;
 
