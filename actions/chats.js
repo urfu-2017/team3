4b2d4886fa2591bc4e1f5fbe7086893d8cee0902
Artem Borzov <artem.borz@yandex.ru>
2018-05-18 03:52:01 +0500

    sound and vibration

--- actions/chats.js
+++ actions/chats.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import Chat from '../models/Chat';
+import { notifyMessage } from '../utils/notification';
 import getSocket from '../pages/socket';
 
 import types from './types';
@@ -55,10 +55,16 @@ export const receiveChat = chat => dispatch => {
 
 export const receiveMessage = (chatId, message) => (dispatch, getState) => {
     const { user, chats } = getState();
-    const chat = new Chat(chats.find(c => c._id === chatId));
+    const chat = chats.find(c => c._id === chatId);
 
     dispatch({ type: types.RECEIVE_MESSAGE, chatId, message });
-    notifyMessage({ message, chat, user, dispatch });
+
+    const onclick = () => {
+        window.focus();
+        dispatch(openChat(chat._id));
+    };
+
+    notifyMessage({ message, chat, user, dispatch, onclick });
 };
 
 export const updateMessage = (chatId, message) => dispatch => {
@@ -72,42 +78,3 @@ export const updateChat = chat => dispatch => {
 export const destructMessage = (chatId, messageId) => dispatch => {
     dispatch({ type: types.DESTRUCT_MESSAGE, chatId, messageId });
 };
-
-function notifyMessage({ message, chat, user, dispatch }) {
-    const chatInfo = chat.type === 'group' ? ` in ${chat.title}` : '';
-    const title = `@${message.author}${chatInfo}`;
-    const body = message.forwardFrom
-        ? message.forwardFrom.text
-        : message.text;
-    const icon = chat.getAvatarFor(user).replace('.svg', '.png');
-
-    const onclick = () => {
-        window.focus();
-        dispatch(openChat(chat._id));
-    };
-
-    notify({ title, body, icon, onclick });
-}
-
-function notify({ title, body, icon, sound, onclick, vibrate, checkVisibility = true }) {
-    if (checkVisibility && !document.hidden) {
-        return;
-    }
-
-    if (!('Notification' in window)) {
-        return;
-    }
-
-    Notification.requestPermission(permission => {
-        if (permission === 'granted') {
-            const notification = new Notification(title, {
-                icon,
-                body,
-                sound,
-                vibrate
-            });
-
-            notification.onclick = () => onclick();
-        }
-    });
-}
---
+++ utils/notification.js
@@ -0,0 +1,52 @@
+'use strict';
+
+import Chat from '../models/Chat';
+
+const NEWMSG_SOUND_URL = '/static/newmsg.wav';
+const NEWMSG_VIBRATION_PATTERN = [100, 100];
+
+/* eslint-disable-next-line */
+export const notifyMessage = ({ message, chat, user, onclick }) => {
+    chat = new Chat(chat);
+    const chatInfo = chat.type === 'group' ? ` in ${chat.title}` : '';
+    const title = `@${message.author}${chatInfo}`;
+    const body = message.forwardFrom
+        ? message.forwardFrom.text
+        : message.text;
+    const icon = chat.getAvatarFor(user).replace('.svg', '.png');
+
+    notifySound(NEWMSG_SOUND_URL);
+    notifyVibration(NEWMSG_VIBRATION_PATTERN);
+    if (document.hidden) {
+        notifyScreen({ title, body, icon, onclick });
+    }
+};
+
+function notifySound(soundUrl) {
+    if ('Audio' in window) {
+        new Audio(soundUrl).play();
+    }
+}
+
+function notifyVibration(vibration) {
+    if ('navigator' in window && navigator.vibrate) {
+        navigator.vibrate(vibration);
+    }
+}
+
+function notifyScreen({ title, body, icon, onclick }) {
+    if (!('Notification' in window)) {
+        return;
+    }
+
+    Notification.requestPermission(permission => {
+        if (permission === 'granted') {
+            const notification = new Notification(title, {
+                icon,
+                body
+            });
+
+            notification.onclick = () => onclick();
+        }
+    });
+}
