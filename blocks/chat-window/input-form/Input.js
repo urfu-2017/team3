2d4a6fc1fa07c90011ea65849182cec31104aec2
spt30 <seregatoschilin@gmail.com>
2018-05-20 05:51:23 +0500

    фокус на инпут и картинка смены темы

--- blocks/chat-window/input-form/Input.js
+++ blocks/chat-window/input-form/Input.js
@@ -40,6 +40,10 @@ class Input extends Component {
         this.props.onRef(null);
     }
 
+    componentDidUpdate() {
+        this.textInput.focus();
+    }
+
     // при вводе добавляем с state
     changeText = e => this.setState({ msgText: e.target.value });
 
